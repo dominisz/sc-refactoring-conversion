a0a200dbc0481a540ad6cd42248c3c24ab83fd58
dominisz <dominisz@gmail.com>
2019-07-18 20:36:21 +0200

    Use Google Java Formatter

--- src/main/java/pl/dominisz/FileEncoderService.java
+++ src/main/java/pl/dominisz/FileEncoderService.java
@@ -2,30 +2,29 @@ package pl.dominisz;
 
 public class FileEncoderService {
 
-    private static final Logger log = LoggerFactory.getLogger(FileEncoderService.class);
+  private static final Logger log = LoggerFactory.getLogger(FileEncoderService.class);
 
-    private final EmailClient emailClient;
-    private final FileEncoder fileEncoder;
+  private final EmailClient emailClient;
+  private final FileEncoder fileEncoder;
 
-    public FileEncoderService(EmailClient emailClient, FileEncoder fileEncoder) {
-        this.emailClient = emailClient;
-        this.fileEncoder = fileEncoder;
+  public FileEncoderService(EmailClient emailClient, FileEncoder fileEncoder) {
+    this.emailClient = emailClient;
+    this.fileEncoder = fileEncoder;
+  }
+
+  public Result encode(String sourceFilename, String destinationFilename) {
+
+    log.info("Start encoding " + sourceFilename);
+
+    Result res = fileEncoder.encode(sourceFilename, destinationFilename);
+
+    // If file encoding took too long then send email message
+    if (res.isSuccessful()) {
+      log.info("Stop encoding file " + sourceFilename);
+    } else {
+      emailClient.sendFailedMessage(sourceFilename);
     }
 
-    public Result encode(String sourceFilename, String destinationFilename) {
-
-        log.info("Start encoding " + sourceFilename);
-
-        Result res = fileEncoder.encode(sourceFilename, destinationFilename);
-
-        //If file encoding took too long then send email message
-        if (res.isSuccessful()) {
-            log.info("Stop encoding file " + sourceFilename);
-        } else {
-            emailClient.sendFailedMessage(sourceFilename);
-        }
-
-        return res;
-    }
-
+    return res;
+  }
 }
